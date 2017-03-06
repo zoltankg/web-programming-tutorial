4394737233b6e3a56fad33143d9ef53cac271d69
zoltankg <zoltankarczag77@gmail.com>
2017-03-06 15:40:47 +0200

    move add products into js

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,4 +1,15 @@
 console.info('Shopping list for today');
 
+function getRow (produs, cantitate) {
+
+        return '<tr>' +
+               '<td>' + produs + '</td>' +
+               '<td>' + cantitate + '</td>' +
+               '</tr>';
+
+}
+
+document.getElementsByTagName('tbody')[0].innerHTML = getRow("Paine", 1) + getRow("Suc", 3);
+
 
 
